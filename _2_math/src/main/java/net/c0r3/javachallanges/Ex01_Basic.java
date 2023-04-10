24a0a6ac2418d9a97e6fdacd161ae6e4e0d9413b
Romeo Köppel <romeo98@bluewin.ch>
2023-04-10 23:00:45 +0200

    v1 of 2.2.1 b

--- _2_math/src/main/java/net/c0r3/javachallanges/Ex01_Basic.java
+++ _2_math/src/main/java/net/c0r3/javachallanges/Ex01_Basic.java
@@ -1,8 +1,40 @@
 package net.c0r3.javachallanges;
 
+import java.util.ArrayList;
+
+/**
+ * Exercise 2.2
+ */
 public class Ex01_Basic {
 
-    public static int calc(int m, int n) {
-        return 1;
+  /**
+   * Exercise 2.2.1 a
+   */
+  public static int calc(int m, int n) {
+    return n * m / 2 % 7;
+  }
+
+
+  /**
+   * Exercise 2.2.1 b
+   */
+  record ResultCalcSumAndCount(int sum, int count){}
+  public static ResultCalcSumAndCount calcSumAndCountAllNumberDivBy_2_Or_7(int x) {
+    var foundNumbers = new ArrayList<Integer>();
+
+    for (int i = 1; i < x; i++) {
+
+      if (i % 2 == 0) {
+        foundNumbers.add(i);
+      } else if (i % 7 == 0) {
+        foundNumbers.add(i);
+      }
     }
+
+    var sum =  foundNumbers.stream().reduce(Integer::sum).orElse(0);
+    return new ResultCalcSumAndCount(sum, foundNumbers.size());
+  }
+  
+
+
 }
--- _2_math/src/test/java/net/c0r3/javachallanges/Ex01_BasicTest.java
+++ _2_math/src/test/java/net/c0r3/javachallanges/Ex01_BasicTest.java
@@ -7,15 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Ex01_BasicTest {
 
+  @ParameterizedTest
+  @CsvSource(value = {"6,7,0", "3,4,6", "5,5,5"})
+  void basicTest(int param1, int param2, int expected) {
+    var actual = Ex01_Basic.calc(param1, param2);
+    assertEquals(expected, actual);
+  }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "6,7,0",
-            "3,4,6",
-            "5,5,5"
-    })
-    void basicTest(int param1, int param2, int expected) {
-        var actual = Ex01_Basic.calc(param1, param2);
-        assertEquals(expected, actual);
-    }
+  @ParameterizedTest
+  @CsvSource(value = {"3,1,2", "8,4,19", "15,8,63"})
+  void calcSumAndCountAllNumberDivBy_2_Or_7Test(int x, int count, int sum) {
+    var actual = Ex01_Basic.calcSumAndCountAllNumberDivBy_2_Or_7(x);
+    assertEquals(count, actual.count());
+    assertEquals(sum, actual.sum());
+  }
 }
